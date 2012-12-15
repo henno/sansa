b28b577ed4bc15fb3711b45f70127e288169fb55
Henno Täht <henno.taht@diara.ee>
2012-12-15 14:11:26 +0200

    Renamed welcome controller to dashboard. Added form for tweeting.

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,5 @@
+<p class="lead">Tweet here</p>
+<form class="form-signin" method="post">
+    <textarea name="tweet" rows="3" placeholder="Write your tweet"></textarea><br/>
+    <button class="btn btn-primary" type="submit">Tweet</button>
+</form>
